8104161f1448bf83dfd31cb39f9b55e8d76417c1
tejaswigowda <tejaswi@foxyninjastudios.com>
2021-03-16 16:40:42 -0700

    ac

---
+++ https.js
@@ -0,0 +1,18 @@
+var homeDir = require('path').join(require('os').homedir());
+require('greenlock-express').create({
+  version: 'draft-11'
+, server: 'https://acme-v02.api.letsencrypt.org/directory'
+//, server: 'https://acme-staging-v02.api.letsencrypt.org/directory'  // staging
+, email: 'tejaswi@asu.edu'                                     // CHANGE THIS
+, agreeTos: true
+, approveDomains: [ 'thebetween.info', 'www.thebetween.info' ]              // CHANGE THIS
+, store: require('greenlock-store-fs')
+, configDir: homeDir
+//, app: require('express')().use('/', function (req, res) {
+//    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+//    res.end('Hello, World!\n\n💚 🔒.js');
+//  })
+, app: require('./server.js')
+//, communityMember: true
+}).listen(8080, 8443);
+
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var url = require("url"),
 	querystring = require("querystring");
 var passport = require('passport');
 var fs = require('fs');
-	var dbURL = 'mongodb://34.219.163.158:27017/test';
+var dbURL = 'mongodb://127.0.0.1:27017/test';
 var path = require('path'),
   express = require('express'),
   db = require('mongoskin').db(dbURL);
@@ -24,7 +24,7 @@ app.use(session( {store: new MongoStore({
 app.use(passport.initialize());
 app.use(passport.session());
 var flash = require('express-flash');
-app.use( flash() );
+app.use( flash() );      
 
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
@@ -32,7 +32,7 @@ var methodOverride = require("method-override");
 app.use(methodOverride());
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended:false
+  extended:false 
 }));
 require('./passport/config/passport')(passport); // pass passport for configuration
 require('./passport/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
@@ -47,7 +47,14 @@ app.get("/getProjects", function(req,res){
 })
 
 app.use(express.static(path.join(__dirname, 'docs')));
-app.listen(8080);
+// DO NOT DO app.listen() unless we're testing this directly
+if (require.main === module) { app.listen(8080); }
+// Instead do export the app:
+else{ module.exports = app; }
+
+
+
+
 
 // route middleware to ensure user is logged in
 function isLoggedIn(req, res, next) {
